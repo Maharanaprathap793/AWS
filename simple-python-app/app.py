3485d8ad503afe48dba3fc791946e380246d4152
Maharanaprathap793 <ranaprathap1122@gmail.com>
2024-12-14 20:43:57 +0530

    Update app.py

--- simple-python-app/app.py
+++ simple-python-app/app.py
@@ -1,10 +1,17 @@
+# Import the Flask class from the flask package
 from flask import Flask
 
+# Create an instance of the Flask class. 
+# __name__ is passed to help Flask determine the app's root path.
 app = Flask(__name__)
 
+# Define a route for the root URL ('/') and bind it to the hello() function.
 @app.route('/')
 def hello():
+    # This function is executed when a user accesses the root URL.
+    # It returns a simple "Hello, world!" message as the response.
     return 'Hello, world!'
 
+# Check if the script is being run directly (not imported as a module).
 if __name__ == '__main__':
-    app.run()
+    # Run the Flask dev
