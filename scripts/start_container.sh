5c79aecb6975853c791d531ccb3978cfc3443737
Maharanaprathap793 <ranaprathap1122@gmail.com>
2024-12-15 20:00:20 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+ #!/bin/bash
 set -e
 
 
 docker pull maharanaprathap793/simple-python-flask-app:latest
 
-docker run -d -p 5000:5000
+docker run -d -p 5000:5000 maharanaprathap793/simple-python-flask-app:latest
